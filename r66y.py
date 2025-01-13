cef224d0ee07421b1d81bd9bfb59a3ef1a7c6cbb
Schala <schalaalexiazeal@gmail.com>
2025-01-13 10:12:20 -0800

    Create r66y.py

---
+++ r66y.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+import socket
+import sys
+
+class Character(IntEnum):
+	CRONO = 1
+	MARLE = 2
+	LUCCA = 3
+	FROG = 4
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+	sock.connect((sys.argv[1], 12345))
+	sock.sendall(f"1#0#{Character.FROG}".encode("utf-8"))
